e22bd8209f526ecb79d1cd5efd1b10a1703a3de8
jennyallar <jenny.allar@guildeducation.com>
2023-01-02 20:54:15 -0700

    using turbo frame tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "home/index"
+  get "posts/index"
+
+  get '/home/turbo_frame_form' => 'home#turbo_frame_form', as: 'turbo_frame_form'
+  post '/home/turbo_frame_submit' => 'home#turbo_frame_submit', as: 'turbo_frame_submit'
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "home#index"
 end
