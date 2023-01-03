f79d522a3330805ceae0ae178256e47751152d38
jennyallar <jenny.allar@guildeducation.com>
2023-01-02 21:30:30 -0700

    back to vanilla rails appwith using turbo stream

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,7 @@
 class PostsController < ApplicationController
+  def post_something
+    respond_to do |format|
+      format.turbo_stream {  }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   get "home/index"
   get "posts/index"
 
-  get '/home/turbo_frame_form' => 'home#turbo_frame_form', as: 'turbo_frame_form'
-  post '/home/turbo_frame_submit' => 'home#turbo_frame_submit', as: 'turbo_frame_submit'
-
   # Defines the root path route ("/")
   root to: "home#index"
 end
